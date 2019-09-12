6f01616dfaf22a2b3abea29a0fed894bbb0b5bff
TianchaoHuo <37928758+TianchaoHuo@users.noreply.github.com>
2019-09-13 00:39:02 +1000

    Add files via upload

---
+++ 92_reverselistII.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        int change_len = n - m + 1;
+        ListNode *pre_head = NULL;        
+        ListNode *result = head;
+        while(head && --m) //找出前一个链表
+        {
+            pre_head = head;
+            head = head->next;
+        }
+        ListNode *modify_list_tail = head;
+        ListNode *new_head = NULL;
+        
+        while(head && change_len)
+        {
+            ListNode *next = head->next;
+            head->next=new_head;
+            new_head = head;
+            head = next;
+            change_len--;
+            
+        }
+         modify_list_tail->next = head;
+        if (pre_head)
+        {
+           pre_head->next = new_head;
+        }
+        else
+        {
+            result = new_head;
+        }
+        return result;
+    }
+};
